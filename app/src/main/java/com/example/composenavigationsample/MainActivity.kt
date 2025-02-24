c2e416922e22a731e40d4d4d574467e24a4ac21d
hiroaki404 <nosho404@gmail.com>
2025-02-24 21:30:47 +0900

    format and add compose navigation

--- app/src/main/java/com/example/composenavigationsample/MainActivity.kt
+++ app/src/main/java/com/example/composenavigationsample/MainActivity.kt
@@ -4,44 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composenavigationsample.ui.theme.ComposeNavigationSampleTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             ComposeNavigationSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController,
+                        startDestination = Screen1,
+                    ) {
+                        composable<Screen1> {
+                            Column {
+                                Text("Screen 1")
+                                Button(onClick = {
+                                    navController.navigate(Screen2)
+                                }) {
+                                    Text("Go to Screen 2")
+                                }
+                            }
+                        }
+                        composable<Screen2> {
+                            Column {
+                                Text("Screen 2")
+                                Button(onClick = {
+                                    navController.navigate(Screen1)
+                                }) {
+                                    Text("Go to Screen 1")
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+@Serializable
+object Screen1
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeNavigationSampleTheme {
-        Greeting("Android")
-    }
-}
+@Serializable
+object Screen2
