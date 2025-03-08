91f1ad1de51863447835efe3175a2c8a6019dcb4
hiroaki404 <nosho404@gmail.com>
2025-03-08 12:14:15 +0900

    use safe args in nestedSampleGraph

--- app/src/main/java/com/example/composenavigationsample/navigation/NestedSampleNavigation.kt
+++ app/src/main/java/com/example/composenavigationsample/navigation/NestedSampleNavigation.kt
@@ -4,22 +4,50 @@ import androidx.compose.material3.Text
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 fun NavGraphBuilder.nestedSampleGraph() {
-    navigation(route = "nested_sample", startDestination = "s1") {
-        composable("s1") {
+    navigation<NestedSampleRoute.NestedGraph>(startDestination = NestedSampleRoute.NestedSampleScreen1) {
+        composable<NestedSampleRoute.NestedSampleScreen1> {
             Text("s1")
         }
-        composable("s2") {
+        composable<NestedSampleRoute.NestedSampleScreen2> {
             Text("s2")
         }
-        navigation(route = "nested_sample2", startDestination = "s3") {
-            composable("s3") {
+        navigation<NestedSampleRoute.NestedNestedGraph>(startDestination = NestedSampleRoute.NestedSampleScreen3) {
+            composable<NestedSampleRoute.NestedSampleScreen3> {
                 Text("s3")
             }
-            composable("s4") {
+            composable<NestedSampleRoute.NestedSampleScreen4> {
                 Text("s4")
             }
         }
     }
+}
+
+sealed interface NestedSampleRoute {
+    @Serializable
+    @SerialName("NestedGraph")
+    data object NestedGraph : NestedSampleRoute
+
+    @Serializable
+    @SerialName("NestedSampleScreen1")
+    data object NestedSampleScreen1 : NestedSampleRoute
+
+    @Serializable
+    @SerialName("NestedSampleScreen2")
+    data object NestedSampleScreen2 : NestedSampleRoute
+
+    @Serializable
+    @SerialName("NestedNestedGraph")
+    data object NestedNestedGraph : NestedSampleRoute
+
+    @Serializable
+    @SerialName("NestedSampleScreen3")
+    data object NestedSampleScreen3 : NestedSampleRoute
+
+    @Serializable
+    @SerialName("NestedSampleScreen4")
+    data object NestedSampleScreen4 : NestedSampleRoute
 }
